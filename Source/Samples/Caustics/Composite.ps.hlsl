a7543cbd1d8c0a0f3d0563aab727769add966626
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-12-27 23:30:49 +0300

    data files: shaders, pictures, ini

---
+++ Source/Samples/Caustics/Composite.ps.hlsl
@@ -0,0 +1,265 @@
+/***************************************************************************
+# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***************************************************************************/
+//__import ShaderCommon;
+//__import Shading;
+//__import DefaultVS;
+
+import Scene.Raster;
+
+#include "Common.hlsl"
+
+SamplerState gPointSampler : register(s1);
+
+Texture2D gDepthTex;
+Texture2D gNormalTex;
+Texture2D gDiffuseTex;
+Texture2D gSpecularTex;
+Texture2D gPhotonTex;
+Texture2D gRaytracingTex;
+Texture2D<float4> gRayTex;
+Texture1D<uint> gStatisticsTex;
+Texture2D<uint> gSmallPhotonTex;
+StructuredBuffer<uint4> gRayCountQuadTree;
+StructuredBuffer<PixelInfo> gPixelInfo;
+
+// Debug modes
+#define ShowDepth       1
+#define ShowNormal      2
+#define ShowDiffuse     3
+#define ShowSpecular    4
+#define ShowPhoton      5
+#define ShowWorld       6
+#define ShowRoughness   7
+#define ShowRayTex      8
+#define ShowRayTracing  9
+#define ShowAvgScreenArea 10
+#define ShowAvgScreenAreaVariance 11
+#define ShowCount 12
+#define ShowTotalPhoton 13
+#define ShowRayCountMipTex 14
+#define ShowPhotonDensity 15
+#define ShowSmallPhotonTex 16
+#define ShowSmallPhotonCount 17
+
+cbuffer PerImageCB
+{
+    // Lighting params
+    LightData gLightData[16];
+    float4x4 gInvWvpMat;
+    float4x4 gInvPMat;
+
+    float3 gCameraPos;
+    uint gNumLights;
+
+    int2 screenDim;
+    int2 dispatchSize;
+
+    uint gDebugMode;
+    float gMaxPixelArea;
+    int gRayTexScale;
+    uint gStatisticsOffset;
+
+    float gMaxPhotonCount;
+    int gRayCountMip;
+    float gSmallPhotonColorScale;
+};
+
+VSOut vsMain(VSIn vIn)
+{
+    return defaultVS(vIn);
+}
+
+float4 psMain(VSOut vsOut, uint triangleIndex : SV_PrimitiveID) : SV_TARGET
+{
+    float depth = gDepthTex.Sample(gPointSampler, texC).r;
+    float4 screenPnt = float4(texC * float2(2, -2) + float2(-1, 1), depth, 1);
+    float4 worldPnt = mul(screenPnt, gInvWvpMat);
+    worldPnt /= worldPnt.w;
+    float4 normalVal = gNormalTex.Sample(gPointSampler, texC);
+    float4 diffuseVal = gDiffuseTex.Sample(gPointSampler, texC);
+    float4 specularVal = gSpecularTex.Sample(gPointSampler, texC);
+    float4 rtColor = gRaytracingTex.Sample(gPointSampler, texC);
+
+    float4 color = rtColor;
+    if (gDebugMode == ShowDepth)
+    {
+        float4 viewPnt = mul(screenPnt, gInvPMat);
+        viewPnt /= viewPnt.w;
+        float viewDepth = (gInvPMat[2][2] * depth + gInvPMat[3][2]) / (gInvPMat[2][3] * depth + gInvPMat[3][3]);
+        color = float4(abs(viewDepth.xxx) * 0.01, 1);
+    }
+    else if (gDebugMode == ShowNormal)
+        color = normalVal;
+    else if (gDebugMode == ShowDiffuse)
+        color = gDiffuseTex.Sample(gPointSampler, texC);
+    else if (gDebugMode == ShowSpecular)
+        color = gSpecularTex.Sample(gPointSampler, texC);
+    else if (gDebugMode == ShowWorld)
+        color = frac(worldPnt * 0.01 + 0.01);
+    else if (gDebugMode == ShowRoughness)
+        color = diffuseVal.a;
+    else if (gDebugMode == ShowRayTex)
+    {
+        int2 screenPixel = texC * screenDim;
+        //float2 uv = clamp(screenPixel / 2048, 0, 1);
+        //color = gRayTex.Sample(gPointSampler, uv);
+        float4 v = gRayTex.Load(int3(screenPixel.xy / gRayTexScale, 0));
+        //color = float4(v >> 16, 0, 0, v & 0xff);
+        //color /= color.w;
+        //color.r /= gMaxPixelArea;
+        if (v.r <= gMaxPixelArea)
+            color = float4(v.rgb / gMaxPixelArea, 1);
+        else
+            color = float4(1, 0, 1, 1);
+    }
+    else if (gDebugMode == ShowRayCountMipTex)
+    {
+        int2 screenPixel = texC * screenDim;
+        int2 texelPos = screenPixel.xy / gRayTexScale;
+        if (all(texelPos < getMipSize(gRayCountMip)))
+        {
+            int offset = getTextureOffset(texelPos.xy, gRayCountMip);
+            uint4 v = gRayCountQuadTree[offset]; // .Load(int3(texelPos, gRayCountMip));
+            uint sum = v.a;
+            if (sum <= gMaxPixelArea)
+                color = float4(sum / gMaxPixelArea, 1);
+            else
+                color = float4(1, 0, 1, 1);
+        }
+    }
+    else if (gDebugMode == ShowRayTracing)
+    {
+        color = rtColor;
+    }
+    else if (gDebugMode == ShowAvgScreenArea || gDebugMode == ShowAvgScreenAreaVariance || gDebugMode == ShowCount)
+    {
+        int2 screenPixel = texC * screenDim;
+        int2 texelPos = screenPixel.xy / gRayTexScale;
+        if (all(texelPos < dispatchSize))
+        {
+            PixelInfo info = gPixelInfo[texelPos.y * dispatchSize.x + texelPos.x];
+            float value = 0;
+            float avgArea = float(info.screenArea) / float(info.count + 0.001);
+            float avgAreaSq = float(info.screenAreaSq) / float(info.count + 0.001);
+            if (gDebugMode == ShowAvgScreenArea)
+            {
+                value = avgArea;
+            }
+            else if (gDebugMode == ShowAvgScreenAreaVariance)
+            {
+                value = sqrt(avgAreaSq - avgArea * avgArea);
+            }
+            else if (gDebugMode == ShowCount)
+            {
+                value = info.count;
+            }
+            float maxVal = gMaxPixelArea;
+            if (value <= maxVal)
+                color = float4(value.xxx / maxVal, 1);
+            else
+                color = float4(1, 0, 1, 1);
+        }
+    }
+    else if (gDebugMode == ShowTotalPhoton)
+    {
+        texC.y = 1 - texC.y;
+        int2 screenPixel = texC * screenDim;
+        int texelPos = (gStatisticsOffset - screenPixel.x + screenDim.x) % screenDim.x;
+        uint valueI = gStatisticsTex[texelPos].r;
+        valueI = valueI * screenDim.y / gMaxPhotonCount;
+        uint segmentWidth = screenDim.y / 4;
+        float4 graphColor;
+        float alpha = 0.3;
+        if (screenPixel.y % segmentWidth == 0)
+            graphColor = float4(0.2, 0.2, 0.2, alpha);
+        else if (screenPixel.y <= valueI)
+            graphColor = float4(1, 0, 0, alpha);
+        else
+            graphColor = float4(0, 0, 0, 0);
+        color.rgb = lerp(rtColor.rgb, graphColor.rgb, graphColor.a);
+        color.a = 1;
+    }
+    else if (gDebugMode == ShowSmallPhotonTex)
+    {
+        texC.y = 1 - texC.y;
+        int2 screenPixel = texC * screenDim;
+        uint v = gSmallPhotonTex.Load(int3(screenPixel.xy, 0));
+        float4 photonClr = decompressColor(v, gSmallPhotonColorScale);
+        color = float4(photonClr.rgb, 1);
+    }
+    else if (gDebugMode == ShowSmallPhotonCount)
+    {
+        texC.y = 1 - texC.y;
+        int2 screenPixel = texC * screenDim;
+        uint v = gSmallPhotonTex.Load(int3(screenPixel.xy, 0));
+        float4 photonClr = decompressColor(v, gSmallPhotonColorScale);
+        if (photonClr.a <= gMaxPixelArea)
+            color = float4(photonClr.aaa / gMaxPixelArea, 1);
+        else
+            color = float4(1, 0, 1, 1);
+        color.rgb += rtColor.rgb; // *0.5;
+    }
+    else
+    {
+        ShadingData sd = initShadingData();
+        sd.posW = worldPnt.xyz;
+        sd.V = normalize(gCameraPos - sd.posW);
+        sd.N = normalVal.xyz;
+        sd.NdotV = saturate(dot(sd.V, sd.N));
+        sd.linearRoughness = diffuseVal.a;
+        sd.roughness = sd.linearRoughness * sd.linearRoughness;
+        sd.specular = specularVal.xyz;
+        sd.diffuse = diffuseVal.rgb;
+        sd.opacity = specularVal.a;
+        color = float4(0, 0, 0, 1);
+        for (uint l = 0; l < gNumLights; l++)
+        {
+            ShadingResult sr = evalMaterial(sd, gLightData[l], 1);
+            color.rgb += sr.color.rgb;
+        }
+
+        float4 photonClr = gPhotonTex.Sample(gPointSampler, texC);
+        if (gDebugMode == ShowPhoton)
+        {
+            color.rgb = photonClr.rgb; // lerp(color.rgb, photonClr.rgb, photonClr.a);
+        }
+        else if (gDebugMode == ShowPhotonDensity)
+        {
+            if (photonClr.a <= gMaxPixelArea)
+                color.rgb = photonClr.aaa / gMaxPixelArea;
+            else
+                color = float4(1, 0, 1, 1);
+        }
+        else
+        {
+            color.rgb += photonClr.rgb * sd.diffuse;
+        }
+    }
+
+    return color;
+}
---
+++ Source/Samples/Caustics/CompositeRT.rt.hlsl
@@ -0,0 +1,258 @@
+/***************************************************************************
+# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***************************************************************************/
+#include "Common.hlsl"
+RWTexture2D<float4> gOutput;
+Texture2D<float4> gCausticsTex;
+Texture2D gDepthTex;
+Texture2D gNormalTex;
+SamplerState gLinearSampler;
+SamplerState gPointSampler;
+__import Raytracing;
+import Helpers;
+
+/*shared*/ cbuffer PerFrameCB
+{
+    float4x4 invView;
+    float4x4 invProj;
+    float2 viewportDims;
+    float tanHalfFovY;
+    uint sampleIndex;
+    bool useDOF;
+    float roughThreshold;
+    int maxDepth;
+    float iorOverride;
+    int causticsResRatio;
+    float gPosKernel;
+    float gZKernel;
+    float gNormalKernel;
+};
+
+struct HitData
+{
+    float3 color;
+    float hitT;
+    float3 nextDir;
+    float3 throughput;
+};
+
+struct ShadowRayData
+{
+    bool hit;
+};
+
+[shader("miss")]
+void shadowMiss(inout ShadowRayData hitData)
+{
+    hitData.hit = false;
+}
+
+[shader("anyhit")]
+void shadowAnyHit(inout ShadowRayData hitData, in BuiltInTriangleIntersectionAttributes attribs)
+{
+    hitData.hit = true;
+}
+
+[shader("miss")]
+void primaryMiss(inout HitData hitData)
+{
+    hitData.color = 0; // float4(0.38f, 0.52f, 0.10f, 1);
+    hitData.hitT = -1;
+    hitData.throughput = 0;
+}
+
+bool checkLightHit(uint lightIndex, float3 origin)
+{
+    float3 direction = gScene.getLight(lightIndex).posW - origin;
+    RayDesc ray;
+    ray.Origin = origin;
+    ray.Direction = normalize(direction);
+    ray.TMin = 0.001f;
+    ray.TMax = max(0.01f, length(direction));
+
+    ShadowRayData rayData;
+    rayData.hit = true;
+    TraceRay(gRtScene, RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH, 0xFF, 1 /* ray index */, hitProgramCount, 1, ray, rayData);
+    return rayData.hit;
+}
+
+void getSample(SamplerState samplerState, float2 uv, out float3 causticsClr, out float3 normal, out float depth)
+{
+    causticsClr = gCausticsTex.SampleLevel(samplerState, uv, 0).rgb;
+    normal = gNormalTex.SampleLevel(samplerState, uv, 0).rgb;
+    depth = gDepthTex.SampleLevel(samplerState, uv, 0).r;
+
+    depth = toViewSpace(invProj, depth);
+    //float4 screenPnt = float4(uv * float2(2, -2) + float2(-1, 1), depth, 1);
+    //float4 worldPnt = mul(screenPnt, invProj);
+    //pos = worldPnt.xyz / worldPnt.w;
+}
+
+float getWeight(float2 offset, float3 normal0, float3 normal1, float z0, float z1)
+{
+    float normalDiff = gNormalKernel * (1 - saturate(dot(normal0, normal1)));
+    //float3 dPos = pos0 - pos1;
+    float zDiff = gZKernel * abs(z0 - z1); // (length(dPos));
+    float uvDiff = gPosKernel * length(offset);
+    //float x = zDiff * normalDiff * uvDiff * gPosKernel;// *zDiff * 1;
+    return exp(-1 * (zDiff * zDiff + normalDiff * normalDiff + uvDiff * uvDiff));
+}
+
+[shader("closesthit")]
+void primaryClosestHit(inout HitData hitData, in BuiltInTriangleIntersectionAttributes attribs)
+{
+    float3 rayOrigW = WorldRayOrigin();
+    float3 rayDirW = WorldRayDirection();
+    float hitT = RayTCurrent();
+    uint triangleIndex = PrimitiveIndex();
+    
+    // prepare the shading data
+    float3 posW = rayOrigW + hitT * rayDirW;
+    VSOut v = getVertexAttributes(triangleIndex, attribs);
+    ShadingData sd = prepareShadingData(v, gMaterial, rayOrigW, 0);
+
+    float3 color = 0;
+    if (sd.linearRoughness > roughThreshold || sd.opacity < 1)
+    {
+        bool isReflect = (sd.opacity == 1);
+        float3 R;
+        float eta = iorOverride > 0 ? 1.0 / iorOverride : 1.0 / sd.IoR;
+        float3 N = v.normalW;
+        if (!isReflect)
+        {
+            if (dot(N, rayDirW) > 0)
+            {
+                eta = 1.0 / eta;
+                N *= -1;
+            }
+            isReflect = isTotalInternalReflection(rayDirW, N, eta);
+        }
+
+        if (isReflect)
+        {
+            R = reflect(rayDirW, N);
+        }
+        else
+        {
+            getRefractVector(rayDirW, N, R, eta);
+        }
+
+        float3 baseColor = lerp(1, sd.diffuse, sd.opacity);
+        color = 0;
+        hitData.nextDir = R;
+        hitData.throughput = baseColor;
+    }
+    else
+    {
+        float4 posS = mul(float4(posW, 1), gCamera.viewProjMat);
+        posS /= posS.w;
+        if (all(posS.xy < 1))
+        {
+            posS.y *= -1;
+            float2 texCoord = (posS.xy + 1) * 0.5;
+            float3 clr0, nor0;
+            float z0;
+            getSample(gLinearSampler, texCoord, clr0, nor0, z0);
+
+            float totalWeight = 1;
+            float3 causticsClr = clr0;
+            if (gPosKernel > 0 || gZKernel > 0 || gNormalKernel > 0)
+            {
+                float2 dir[] =
+                {
+                    float2(-1, -1), float2(-1, 0), float2(-1, 1),
+                    float2(0, -1), float2(0, 1),
+                    float2(1, -1), float2(1, 0), float2(1, 1),
+                };
+                [unroll]
+                for (int i = 0; i < 8; i++)
+                {
+                    float2 sampleUV = texCoord + dir[i] / (viewportDims / causticsResRatio);
+                    float3 clr, nor;
+                    float z;
+                    getSample(gLinearSampler, sampleUV, clr, nor, z);
+                    float w = getWeight(dir[i], nor0, nor, z0, z);
+                    causticsClr += clr * w;
+                    totalWeight += w;
+                    //color += w;
+                }
+            }
+            causticsClr /= totalWeight;
+
+            color += sd.diffuse * causticsClr; // gCausticsTex.SampleLevel(gLinearSampler, texCoord, 0).rgb;
+        }
+        [unroll]
+        for (int i = 0; i < gLightsCount; i++)
+        {
+            if (checkLightHit(i, posW) == false)
+            {
+                color += evalMaterial(sd, gLights[i], 1).color.xyz;
+            }
+        }
+
+        hitData.throughput = 0;
+    }
+
+    hitData.color.rgb = color;
+    hitData.hitT = hitT;
+    hitData.color.rgb += sd.emissive;
+}
+
+[shader("raygeneration")]
+void rayGen()
+{
+    uint3 launchIndex = DispatchRaysIndex();
+    uint randSeed = rand_init(launchIndex.x + launchIndex.y * viewportDims.x, sampleIndex, 16);
+
+    RayDesc ray;
+    if (!useDOF)
+    {
+        ray = generateRay(gCamera, launchIndex.xy, viewportDims);
+    }
+    else
+    {
+        ray = generateDOFRay(gCamera, launchIndex.xy, viewportDims, randSeed);
+    }
+
+    float3 totalColor = 0;
+    float3 totalThroughput = 1;
+    for (int i = 0; i < maxDepth && any(totalThroughput > 0); i++)
+    {
+        HitData hitData;
+        TraceRay(gRtScene, 0, 0xFF, 0, hitProgramCount, 0, ray, hitData);
+
+        totalColor += totalThroughput * hitData.color.rgb;
+        totalThroughput *= hitData.throughput;
+
+        ray.Origin = ray.Origin + ray.Direction * hitData.hitT;
+        ray.Direction = hitData.nextDir;
+        ray.TMin = 0.001;
+        ray.TMax = 100000;
+    }
+
+    gOutput[launchIndex.xy] = float4(totalColor, 1);
+}
